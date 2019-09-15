7adb3ca756ba22ed4d6f01947f0adfd42e3451c9
Firealem Erko <firealemg@yahoo.com>
2019-09-15 17:56:21 -0400

    components added

---
+++ context_hooks/src/components/BookList.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class BookList extends Component {
+    render() {
+        return (
+            <div className='book-list'>
+               <li>list 1</li>
+               <li>list 2</li>
+               <li>list 3</li>
+            </div>
+        );
+    }
+}
+
+export default BookList;
